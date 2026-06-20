e6c0b2a14277f4e5fe464f3bebe47a28e6d1b79c
ASWG08 <nadhifahnaff@gmail.com>
2026-06-20 17:10:37 +0700

    revisi page login

--- lib/pages/login/components/body.dart
+++ lib/pages/login/components/body.dart
@@ -5,147 +5,139 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ValueNotifier<bool> isObscureNotifier = ValueNotifier<bool>(true);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
-      home: const Login(),
-    );
-  }
-}
-
-class Login extends StatefulWidget {
-  const Login({super.key});
-
-  @override
-  State<Login> createState() => _LoginState();
-}
-
-class _LoginState extends State<Login> {
-  bool _isObscure = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 30.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 60),
-              const Text(
-                'Masuk',
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFFE31E24),
-                ),
-              ),
-              const Text(
-                'Silahkan masuk ke akun',
-                style: TextStyle(fontSize: 16, color: Colors.black54),
-              ),
-
-              const SizedBox(height: 40),
-              //Logo
-              Center(
-                child: Container(
-                  height: 150,
-                  width: 150,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    shape: BoxShape.circle,
-                  ),
-                  child: const Center(
-                    child: Text(
-                      'Logo',
-                      style: TextStyle(color: Colors.black45),
-                    ),
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 60),
+                const Text(
+                  'Masuk',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFFE31E24),
                   ),
                 ),
-              ),
-
-              const SizedBox(height: 50),
-
-              _inputLabel(Icons.person_outline, 'Nama Pengguna / Email'),
-              const SizedBox(height: 8),
-              TextFormField(decoration: _inputDecoration()),
-
-              const SizedBox(height: 20),
-
-              _inputLabel(Icons.lock_outline, 'Password'),
-              const SizedBox(height: 8),
-              TextFormField(
-                obscureText: _isObscure,
-                decoration: _inputDecoration(
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      _isObscure
-                          ? Icons.visibility_off_outlined
-                          : Icons.visibility_outlined,
-                      color: Colors.black54,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _isObscure = !_isObscure;
-                      });
-                    },
-                  ),
+                const Text(
+                  'Silahkan masuk ke akun',
+                  style: TextStyle(fontSize: 16, color: Colors.black54),
                 ),
-              ),
 
-              const SizedBox(height: 40),
-              //Login Button
-              SizedBox(
-                width: double.infinity,
-                height: 55,
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Logika login
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFFE31E24),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
+                const SizedBox(height: 40),
+                // Logo
+                Center(
+                  child: Container(
+                    height: 150,
+                    width: 150,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      shape: BoxShape.circle,
                     ),
-                  ),
-                  child: const Text(
-                    'Masuk',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 25),
-              //Buat Akun Button
-              Center(
-                child: Column(
-                  children: [
-                    const Text(
-                      'Belum memiliki akun?',
-                      style: TextStyle(color: Colors.black54),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(context, '/daftar');
-                      },
-                      child: const Text(
-                        'Buat Akun',
-                        style: TextStyle(
-                          color: Color(0xFFE31E24),
-                          fontWeight: FontWeight.bold,
-                        ),
+                    child: const Center(
+                      child: Text(
+                        'Logo',
+                        style: TextStyle(color: Colors.black45),
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            ],
+
+                const SizedBox(height: 50),
+
+                _inputLabel(Icons.person_outline, 'Nama Pengguna / Email'),
+                const SizedBox(height: 8),
+                TextFormField(decoration: _inputDecoration()),
+
+                const SizedBox(height: 20),
+
+                _inputLabel(Icons.lock_outline, 'Password'),
+                const SizedBox(height: 8),
+                
+                ValueListenableBuilder<bool>(
+                  valueListenable: isObscureNotifier,
+                  builder: (context, isObscure, child) {
+                    return TextFormField(
+                      obscureText: isObscure,
+                      decoration: _inputDecoration(
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            isObscure
+                                ? Icons.visibility_off_outlined
+                                : Icons.visibility_outlined,
+                            color: Colors.black54,
+                          ),
+                          onPressed: () {
+                            // Mengubah status true/false langsung tanpa setState
+                            isObscureNotifier.value = !isObscureNotifier.value;
+                          },
+                        ),
+                      ),
+                    );
+                  },
+                ),
+
+                const SizedBox(height: 40),
+                // Login Button
+                SizedBox(
+                  width: double.infinity,
+                  height: 55,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Logika login
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFE31E24),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    child: const Text(
+                      'Masuk',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 25),
+                // Buat Akun Button
+                Center(
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Belum memiliki akun?',
+                        style: TextStyle(color: Colors.black54),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/daftar');
+                        },
+                        child: const Text(
+                          'Buat Akun',
+                          style: TextStyle(
+                            color: Color(0xFFE31E24),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
           ),
         ),
       ),
